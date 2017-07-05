886f31f6633189f64fddae092890f8c86136b4bb
Piseth Kem <pisethkem@gmail.com>
2017-07-05 15:15:04 +0700

    Add a google map in the about page

--- _view/about.html.php
+++ _view/about.html.php
@@ -10,4 +10,33 @@ Benyamin Business Co., Ltd. is a professional company specializing in business r
 </p>
 SECTION;
 
+$article = <<<"ARTICLE"
+<div id="googleMap" style="width:100%;height:400px;"></div>
+ARTICLE;
+
+$js = <<<"JS"
+<script>
+    function getMap() {
+        var center = new google.maps.LatLng(11.586706,104.843967);
+        var mapProp = {
+            center: center,
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+        };
+        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+        var marker = new google.maps.Marker({
+            position: center,
+            animation: google.maps.Animation.BOUNCE,
+        });
+        marker.setMap(map);
+
+        var infowindow = new google.maps.InfoWindow({
+            content: "Welcome!"
+        });
+        infowindow.open(map,marker);
+    }
+</script>
+<script defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyACYDVau-wlf-OHsb1KIWHYQ22ex9Hq8ps&callback=getMap"></script>
+JS;
+
 require __DIR__ . '/_base_/_base_.html.php';
