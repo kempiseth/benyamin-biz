369625380ddd7e24b9c9d1e1fe6c4a08358c4c55
Piseth Kem <pisethkem@gmail.com>
2017-10-11 10:27:52 +0700

    Update real estate section: land 32 hecta

--- _view/services.html.php
+++ _view/services.html.php
@@ -30,7 +30,20 @@ $section = <<<"SECTION"
 </div>
 <div class="section" id="r-e">
   <div class="header">Real estate services</div>
-  <div class="inner-text"></div>
+  <div class="inner-text">
+    <h4>32 hecta of land for sale along 380 road</h4>
+    <h5>ដីសំរាប់លក់៣២ហិចតា តាមបណ្ដោយ ផ្លូវ៣៨០</h5>
+    <img src="/static/image/land-left.png" width="97%">
+    <img src="/static/image/land-right.png" width="97%">
+Asking price: 13$ / sqm <br>
+Land size: 322,356 m<sup>2</sup> <br>
+Hard Title Deed <br>
+Location: 2km from Prek Tamak Circle or 18Km from Svay Chrum <br>
+Map: <br>
+    <img src="/static/image/land-map.png" width="97%">
+Land structure: <br>
+    <img src="/static/image/land-structure.png" width="97%">
+  </div>
 </div>
 <div class="section" id="qip">
   <div class="header">QIP (Qualified Investment Project) consultation and application</div>
